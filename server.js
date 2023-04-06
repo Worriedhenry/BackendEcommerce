e93212c5b3bb6b3348515b83e8e7a5f4352eb68c
Ankit <kaushikankitsharma05@gmail.com>
2023-04-06 23:00:26 +0530

    Initial Setup Done

---
+++ server.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const cors=require("cors")
+const mongoose=require('mongoose')
+const app=express()
+require('dotenv').config()
+
+//MiddleWares for app
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Api's
+app.get("/",(req,res)=>{
+    res.status(200).send("Ok")
+})
+
+
+
+
+
+//Server Port assigning and executing
+app.listen(process.env.PORT||3001,()=>{
+    console.log("Server is running at port 3001 ")
+})
