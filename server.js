399e3ab2a251f04acf8ca4099652b8ec09efecdb
Worriedhenry <kaushikankitsharma05@gmail.com>
2023-07-03 13:41:50 +0530

    cors changed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGODB_URI).then(()=>{
 //MiddleWares for app
 const cors=require("cors")
 const corsOptions ={
-    origin:'https://fastkart.onrender.com/', 
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
